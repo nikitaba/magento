09a98821605ae7ed6d254655f13b6c41440160f2
Nik <Nik@MacBook-Air-Admin.local>
2016-10-25 20:12:55 +0300

    First commit

---
+++ app/code/local/Ainstainer/TechTalk/Model/Product.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nik
+ * Date: 10/25/16
+ * Time: 15:16
+ */
+
+class Ainstainer_TechTalk_Model_Product
+{
+
+    public function sayHello() {
+        echo "Hello world!";
+    }
+}
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('display_errors', 1);
+require 'app/bootstrap.php';
+require 'app/Mage.php';
+
+Mage::app('admin')->setUseSessionInUrl(false);
+
+umask(0);
+
+//echo Mage::getModel('techtalk/techLogic')->sayHello();
+
+$product = new Ainstainer_TechTalk_Model_Product();
+
+$product->sayHello();
