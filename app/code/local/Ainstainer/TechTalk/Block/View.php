2b2ee8736fb954a762dcb19b236565418bb66c0d
Nik <Nik@MacBook-Air-Admin.local>
2016-11-07 23:35:58 +0200

    Sixth commit

---
+++ app/code/local/Ainstainer/TechTalk/Block/View.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ainstainer_TechTalk_Block_View extends Mage_Core_Block_Template
+{
+    public function getRequestRecord()
+    {
+        return Mage::getModel('techtalk/contact')->load(2);
+    }
+}
---
+++ app/code/local/Ainstainer/TechTalk/Model/Contact.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ainstainer_TechTalk_Model_Contact extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('techtalk/contact');
+    }
+}
